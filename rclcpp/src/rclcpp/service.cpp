bb4ea52714f6dad5c4bb4d1f8591d46e9b143bfb
seonghun <sh.park@rideflux.com>
2025-12-29 11:57:04 +0900

    [SCNS-1394] Fix to print warn log in case of service TIMEOUT instead of throwing exception.

--- rclcpp/src/rclcpp/service.cpp
+++ rclcpp/src/rclcpp/service.cpp
@@ -28,7 +28,8 @@
 using rclcpp::ServiceBase;
 
 ServiceBase::ServiceBase(std::shared_ptr<rcl_node_t> node_handle)
-: node_handle_(node_handle)
+: node_handle_(node_handle),
+  node_logger_(rclcpp::get_node_logger(node_handle_.get()))
 {}
 
 ServiceBase::~ServiceBase()
